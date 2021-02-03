8b4f9d879490ed1e952f115701626de539870fcf
Rebecca Ellinson <rebecca.ellinson@gmail.com>
2021-02-03 16:18:22 +1100

    Change description

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Rebecca Ellinson'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Rebecca Ellinson'} />
       </Helmet>
       <App />
     </>
